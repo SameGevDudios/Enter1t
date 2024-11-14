ed574271c859624d5bef2d5b2ccef04f597b290f
SameGevDudios <magomed.chapaev1@gmail.com>
2024-11-14 19:35:38 +0300

    Add interaction crosshair

--- Assets/Scripts/Player/KeyPress.cs
+++ Assets/Scripts/Player/KeyPress.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class KeyPress : MonoBehaviour
 {
+    [SerializeField] private GameObject _crosshair;
     [SerializeField] private Transform _aimTransform;
     [SerializeField] private float _reachDistance;
     [SerializeField] private LayerMask _searchMask;
     private RaycastHit _hit;
     private void Update()
     {
-        if (KeyInRange())
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                PressKey();
-            }
-        }
+        bool inRange = KeyInRange();
+        ActivateCrosshair(inRange);
+        if (inRange && Input.GetMouseButtonDown(0))
+            PressKey();
     }
+    private void ActivateCrosshair(bool active) =>
+        _crosshair.SetActive(active);
     private bool KeyInRange()
     {
         return Physics.Raycast(_aimTransform.position, _aimTransform.forward, out _hit, _reachDistance, _searchMask);
