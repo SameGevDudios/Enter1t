9f1de181720c64e680afd9b6499e8f8199056407
SameGevDudios <magomed.chapaev1@gmail.com>
2024-11-14 22:24:45 +0300

    Add test run of python program

--- Assets/Scripts/Python/RunPython.cs
+++ Assets/Scripts/Python/RunPython.cs
@@ -1,6 +1,41 @@
 using UnityEngine;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
 
 public class RunPython : MonoBehaviour
 {
 
+    private ScriptEngine _scriptEngine;
+    private ScriptScope _scriptScope;
+
+    #region Singleton
+    public static RunPython Instance;
+    private void Awake() =>
+        Instance = this;
+    #endregion
+
+    private void Start()
+    {
+        InitializePython();
+        RunCode("print('Hello Unity!')");
+    }
+    private void InitializePython()
+    {
+        _scriptEngine = Python.CreateEngine();
+        _scriptScope = _scriptEngine.CreateScope();
+    }
+    public void RunCode(string code)
+    {
+        try
+        {
+            ScriptSource source = _scriptEngine.CreateScriptSourceFromString(code);
+            source.Compile();
+            source.Execute(_scriptScope);
+            Debug.Log("IronPython Script Executed Successfully.");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Error executing Python script: " + ex.Message);
+        }
+    }
 }
