bdd7333bfab1422f30270da7f74ec42399cced92
SameGevDudios <magomed.chapaev1@gmail.com>
2024-11-14 19:24:07 +0300

    Add animation call

--- Assets/Scripts/Computer/Key.cs
+++ Assets/Scripts/Computer/Key.cs
@@ -3,7 +3,11 @@ using UnityEngine.Events;
 
 public class Key : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
     [SerializeField] private UnityEvent _pressEvent;
-    public void Press() =>
+    public void Press()
+    {
+        _animator.SetTrigger("Press");
         _pressEvent?.Invoke();
+    }
 }
