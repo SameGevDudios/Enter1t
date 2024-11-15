8a45aed4e6ab749fca5c6830ec7850b77782aa63
SameGevDudios <magomed.chapaev1@gmail.com>
2024-11-15 19:17:38 +0300

    Add pause

---
+++ Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private PlayerController _playerMovement;
+    [SerializeField] private GameObject _pausePanel;
+    private bool _paused;
+    private void Update() =>
+        CheckPause();
+    private void CheckPause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Pause();
+    }
+    private void Pause()
+    {
+        _paused = !_paused;
+        _playerMovement.enabled = !_paused;
+        _pausePanel.SetActive(_paused);
+    }
+}
